ad8b4abc1ff75bd75afceea1dc70ad8c7fb57c1c
withPain <krushnalokare97991@gmail.com>
2023-09-22 14:45:45 +0530

    connecting backend url to frontend

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://face-detection1.onrender.com/register', {
+    fetch('https://face-detection-backend-uouw.onrender.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://face-detection1.onrender.com/signin', {
+    fetch('https://face-detection-backend-uouw.onrender.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
